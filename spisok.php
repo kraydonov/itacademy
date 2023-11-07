5d06c3e8c5272549a9f94464955ac984554d145d
kraydonov <63381589+kraydonov@users.noreply.github.com>
2023-11-07 19:55:06 +0300

    Add files via upload

---
+++ spisok.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+   <title>!DOCTYPE</title>
+   <meta charset="utf-8">
+ </head>
+ <body>
+   <ul>
+  <li>Оранжевый</li>
+  <li>Зелёный</li>
+  <li>Синий</li>
+</ul>
+ </body> 
+</html>
+
